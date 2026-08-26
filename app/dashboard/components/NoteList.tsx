e2fbf62893c5317455ed891d7ef68bf8f89a7c05
jess <jessica.response201@gmail.com>
2026-08-26 12:22:33 +0200

    delete notes
    Add the ability to delete notes

--- app/dashboard/components/NoteList.tsx
+++ app/dashboard/components/NoteList.tsx
@@ -3,8 +3,9 @@
 import styles from "../dashboard.module.css";
 import { useEffect, useState } from "react";
 import { db } from "@/lib/firebase";
-import { addDoc, collection, getDocs, onSnapshot } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs, onSnapshot } from "firebase/firestore";
 import { title } from "process";
+import { Trash2 } from "lucide-react";
 
 interface NoteData {
 	title: string;
@@ -33,12 +34,26 @@ export default function NoteList() {
 		return unsub;
 	}, []);
 
+	const handleDelete = async (id: string) => {
+		try {
+			await deleteDoc(doc(db, "notes", id));
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className={styles.noteList}>
 			{notes.map((note) => (
 				<div key={note.id} className={styles.noteCard}>
 					<h3 className={styles.noteCardTitle}>{note.title}</h3>
 					<p className={styles.noteCardContent}>{note.content}</p>
+
+					<div className={styles.noteCardFooter}>
+						<button className={styles.deleteButton} onClick={() => handleDelete(note.id)}>
+							<Trash2 size={15} />
+						</button>
+					</div>
 				</div>
 			))}
 		</div>
